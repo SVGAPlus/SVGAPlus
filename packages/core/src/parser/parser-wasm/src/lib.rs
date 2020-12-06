f4e29386974d0ea14cdaa9ddc2a618ad0755022f
LancerComet <chw644@hotmail.com>
2020-12-06 20:29:41 +0800

    + Add inflate decompression for svga file.

--- packages/core/src/parser/parser-wasm/src/lib.rs
+++ packages/core/src/parser/parser-wasm/src/lib.rs
@@ -3,21 +3,30 @@
 mod lib_test;
 mod protos;
 
-// use wasm_bindgen::prelude::*;
-// use wasm_bindgen::__rt::std::io::BufReader;
+use wasm_bindgen::prelude::*;
+use wasm_bindgen::__rt::std::io::BufReader;
+use inflate::inflate_bytes_zlib;
+use protobuf::parse_from_bytes;
+
+use crate::protos::svga::MovieEntity;
+
+#[wasm_bindgen]
+extern {
+  #[wasm_bindgen(js_namespace = console)]
+  fn log (content: &str);
+}
+
+macro_rules! console_log {
+  // Note that this is using the `log` function imported above during
+  // `bare_bones`
+  ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
+}
 
 // #[wasm_bindgen]
-// extern {
-//   #[wasm_bindgen(js_namespace = console)]
-//   fn log (content: &str);
-// }
-
-// macro_rules! console_log {
-//   // Note that this is using the `log` function imported above during
-//   // `bare_bones`
-//   ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
-// }
-
-// #[wasm_bindgen]
-// pub fn decode(bytes: &[u8]) -> MovieEntity {
-// }
+pub fn decode_svga(svga_bytes: &[u8]) -> MovieEntity {
+  let decoded_bytes = inflate_bytes_zlib(&svga_bytes)
+    .expect("failed to inflate svga file");
+  let movie = parse_from_bytes::<MovieEntity>(&decoded_bytes)
+    .expect("failed to parse MovieEntity");
+  return movie;
+}
--- packages/core/src/parser/parser-wasm/src/lib_test.rs
+++ packages/core/src/parser/parser-wasm/src/lib_test.rs
@@ -1,24 +1,25 @@
 #[cfg(test)]
 mod lib_test {
   use std::fs;
-  use protobuf::{
-    parse_from_bytes
-  };
+  use protobuf::parse_from_bytes;
+  use inflate::inflate_bytes_zlib;
 
-  use crate::protos::svga::{
-    MovieEntity
-  };
+  use crate::protos::svga::MovieEntity;
+  use crate::decode_svga;
 
   #[test]
   fn test_decode () {
     let test_file_name = "test.svga";
-
     let test_file_buffer = fs::read(test_file_name)
       .expect("failed to read test.svga");
 
-    let movie = parse_from_bytes::<MovieEntity>(&test_file_buffer)
-      .expect("failed to parse");
+    let movie = decode_svga(&test_file_buffer);
+    assert_eq!(movie.version, String::from("2.1.0"));
 
-    println!("{:?}", &movie);
+    let sprites = movie.sprites.to_vec();
+    assert_eq!(sprites.len(), 11);
+
+    let images = movie.images;
+    assert_eq!(images["img_2509"].len(), 81393);
   }
 }
